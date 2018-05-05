bc8a92fd2ababbba69582a12940438725db492c3
pallavi0427 <38836262+pallavi0427@users.noreply.github.com>
2018-05-05 11:19:00 +0530

    added a new file

---
+++ EmployeeOffice.java
@@ -0,0 +1,19 @@
+
+public class EmployeeOffice {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		Employee e2= new Employee();
+Employee e3= new Employee();
+e1.name ="pallavi"; e1.email = "abc";e1.empid=123;
+e2.name= "neha"; e2.email = "mno"; e2.empid=567;
+e3.name= "neha"; e3.email = "mno"; e3.empid=567;
+System.out.println(e1.name);
+System.out.println(e2.email);
+System.out.println(e3.empid);
+
+	
+
+	}
+
+}
